ced41bb27e8b91bb9c4ea88dfa913dfd3829bdbb
splx <splaandrey@gmail.com>
2021-07-30 20:29:34 +0300

    squash! Added simple PostsController test

--- tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
+++ tests/Feature/Http/Controllers/Auth/LoginControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Http\Controllers\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,6 +10,8 @@ use Tests\TestCase;
 
 class LoginControllerTest extends TestCase
 {
+    use RefreshDatabase;
+
     public function testShowLoginFor(): void
     {
         $response = $this->get(route('login'));
@@ -24,4 +27,18 @@ class LoginControllerTest extends TestCase
 
         $response->assertSessionHasErrors('email');
     }
+    /** @test */
+    public function login_authenticates_and_redirects_user()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->post(route('login'), [
+            'email' => $user->email,
+            'password' => 'password'
+        ]);
+
+        $response->assertRedirect(route('home'));
+        $this->assertAuthenticatedAs($user);
+    }
+
 }
